296bf939c18afa2981a0ca55c1d360219db8e548
Erin Nagoshi <enagoshi@iolani.org>
2015-03-11 10:28:43 -1000

    added two buttons and a label

--- Button Fun/ViewController.swift
+++ Button Fun/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        let title = sender.titleForState(.Normal)!
+        let plainText = "\(title) button pressed"
+        statusLabel.text = plainText
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
