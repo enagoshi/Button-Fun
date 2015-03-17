008aee399c402e0c7d230616b1e568f726f2b798
Erin Nagoshi <enagoshi@iolani.org>
2015-03-16 14:32:17 -1000

    added bold font

--- Button Fun/ViewController.swift
+++ Button Fun/ViewController.swift
@@ -15,7 +15,18 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(sender: UIButton) {
         let title = sender.titleForState(.Normal)!
         let plainText = "\(title) button pressed"
-        statusLabel.text = plainText
+        
+        //statusLabel.text = plainText
+        
+        //creates an attributed string
+        let styledText = NSMutableAttributedString(string: plainText)
+        //says use bold with the same size font as is currently being used.
+        let attributes = [NSFontAttributeName: UIFont.boldSystemFontOfSize(statusLabel.font.pointSize)]
+        //asks plainText string to give us the range of the substring (start index and length)
+        let nameRange = (plainText as NSString).rangeOfString(title)
+        //apply attributes.
+        styledText.setAttributes(attributes, range: nameRange)
+        statusLabel.attributedText = styledText
     }
 }
 
